c826db55c4a36755de7212d34d98bfc7191d837d
emanshehzadi1-cyber <emanshehzadi074@gmail.com>
2026-08-17 02:02:06 +0500

    first commit

---
+++ test3.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+let num1 = Number (prompt("Enter your first number :"));
+let num2 = Number(prompt("Enter your second number :"));
+ let sum;
+ sum = num1 + num2
+console.log("sum =", sum );
