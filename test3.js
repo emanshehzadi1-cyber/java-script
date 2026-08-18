f0fa85ffd55d98403da03189d1ef352605d0dd0b
emanshehzadi1-cyber <emanshehzadi074@gmail.com>
2026-08-19 03:33:15 +0500

    Delete test3.js

--- test3.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-let num1 = Number (prompt("Enter your first number :"));
-let num2 = Number(prompt("Enter your second number :"));
- let sum;
- sum = num1 + num2
-console.log("sum =", sum );
